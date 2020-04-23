5fb2e28f5881d953a7a62684adb55305879e2a7b
jayandran <jaidg2012@gmail.com>
2020-04-24 00:02:19 +0530

    Updated tested to validate

--- src/test/java/io/github/robwin/swagger/SwaggerConsumerDrivenAssertTest.java
+++ src/test/java/io/github/robwin/swagger/SwaggerConsumerDrivenAssertTest.java
@@ -30,12 +30,20 @@ import io.github.robwin.swagger.test.SwaggerAssert;
 import io.github.robwin.swagger.test.SwaggerAssertions;
 import io.swagger.parser.SwaggerParser;
 import org.apache.commons.lang3.Validate;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 
 public class SwaggerConsumerDrivenAssertTest {
 
@@ -205,6 +213,7 @@ public class SwaggerConsumerDrivenAssertTest {
         JsonSchemaValidator schemaValidator = new JsonSchemaValidator(new InputStreamReader(getClass().getResourceAsStream(schema)));
         JsonNode jsonNode = JsonLoader.fromReader(new InputStreamReader(getClass().getResourceAsStream(definition)));
         ProcessingReport report = schemaValidator.validateSchemaWithParams("/definitions/Contacts", jsonNode);
-        Assert.assertTrue(report.isSuccess());
+        Assert.assertFalse(report.isSuccess());
     }
+
 }
